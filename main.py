ee7d003d9f56f8b43ebae6d9a42ac84c876fb0f6
asmithie13 <ajs392@pitt.edu>
2024-04-14 20:32:44 -0400

    changed pyqt5 to pyqt6

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.uic import loadUi
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("StartScreen.ui", self)
+        loadUi("StartScreen.ui", self)
 
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
     main()
